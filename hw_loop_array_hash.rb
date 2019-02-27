4e78c82ec038cd91a11963476ee7fa40aa3fe369
Emily Milne <emily@Emilys-MacBook-Pro.local>
2019-02-27 21:06:15 +0000

    exercise 2, half way

--- hw_loop_array_hash.rb
+++ hw_loop_array_hash.rb
@@ -45,15 +45,115 @@
 # p stops.reverse
 
 # Print out all the stops using a for loop
-stops = [
-  "Croy",
-  "Cumbernauld",
-  "Falkirk High",
-  "Linlithgow",
-  "Livingston",
-  "Edinburgh Waverley",
-]
+# stops = [
+#   "Croy",
+#   "Cumbernauld",
+#   "Falkirk High",
+#   "Linlithgow",
+#   "Livingston",
+#   "Edinburgh Waverley",
+# ]
+#
+# for stop in stops
+#   p "#{stop} is here"
+# end
 
-for stop in stops
-  p "#{stop} is here"
-end
+############# SECOND PART
+
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+
+
+# Get Jonathan's Twitter handle (i.e. the string "jonnyt")
+  p users["Jonathan"][:twitter]
+  # Get Erik's hometown
+  p users["Erik"][:home_town]
+  # Get the array of Erik's lottery numbers
+  p users["Erik"][:lottery_numbers]
+  # Get the type of Avril's pet Monty
+  # p users.fetch("Avril", :pets)[:species]
+# Get the smallest of Erik's lottery numbers
+# Return an array of Avril's lottery numbers that are even
+# Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+# Change Erik's hometown to Edinburgh
+# Add a pet dog to Erik called "Fluffy"
+# Add another person to the users hash
+
+
+####### EXERCISE C
+
+# united_kingdom = [
+#   {
+#     name: "Scotland",
+#     population: 5295000,
+#     capital: "Edinburgh"
+#   },
+#   {
+#     name: "Wales",
+#     population: 3063000,
+#     capital: "Swansea"
+#   },
+#   {
+#     name: "England",
+#     population: 53010000,
+#     capital: "London"
+#   }
+# ]
+# Change the capital of Wales from "Swansea" to "Cardiff".
+
+# Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+# Use a loop to print the names of all the countries in the UK.
+# Use a loop to find the total population of the UK.
