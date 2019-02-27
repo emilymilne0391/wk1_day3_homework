0e69aa2ef4d2826ac5ac4d0e8c8f2ce174ddadd5
Emily Milne <emily@Emilys-MacBook-Pro.local>
2019-02-27 16:24:50 +0000

    many more solved

--- hw_loop_array_hash.rb
+++ hw_loop_array_hash.rb
@@ -1,23 +1,33 @@
 
-# # Add "Edinburgh Waverley" to the end of the array
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-stops.push("Edinburgh Waverley")
-p stops
-
-# Add "Glasgow Queen St" to the start of the array
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-stops.unshift("Glasgow Queen St")
-p stops
-
-# Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-stops.insert(3, "Polmont")
-p stops
+# # # Add "Edinburgh Waverley" to the end of the array
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops.push("Edinburgh Waverley")
+# p stops
+#
+# # Add "Glasgow Queen St" to the start of the array
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops.unshift("Glasgow Queen St")
+# p stops
+#
+# # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops.insert(3, "Polmont")
+# p stops
 
 # Work out the index position of "Linlithgow"
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops.index("Linlithgow")
+# p stops.index("Linlithgow")
 
-# Remove "Livingston" from the array using its name
+# # Remove "Livingston" from the array using its name
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+# stops.delete("Livingston")
+# p stops
+
 # Delete "Cumbernauld" from the array by index
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+stops.delete_at(1)
+p stops
 # How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
