afe941a1259059b3498a2498a24f685890541b7a
Emily Milne <emily@Emilys-MacBook-Pro.local>
2019-02-28 10:31:48 +0000

    updated with results

--- hw_loop_array_hash.rb
+++ hw_loop_array_hash.rb
@@ -2,47 +2,49 @@
 # # # Add "Edinburgh Waverley" to the end of the array
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.push("Edinburgh Waverley")
-p stops
+# p stops
 #
 # # Add "Glasgow Queen St" to the start of the array
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.unshift("Glasgow Queen St")
-p stops
+# p stops
 #
 # # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-stops.insert(3, "Polmont")
-p stops
+stops.insert(4, "Polmont")
+# p stops
+#  OR stops.insert(stops.index)("Linlithgow"),("Polmont")
 
 # Work out the index position of "Linlithgow"
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.index("Linlithgow")
-p stops.index("Linlithgow")
+# p stops.index("Linlithgow")
 
 # # Remove "Livingston" from the array using its name
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.delete("Livingston")
-p stops
+# p stops
 
 # Delete "Cumbernauld" from the array by index
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.delete_at(1)
-p stops
+# p stops
 
 # How many stops there are in the array?
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.count
-p stops.count
+# p stops.count
 
 # How many ways can we return "Falkirk High" from the array?
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops [2]
-p stops [2]
+stops.at(2)
+stops[-5]
 
 # Reverse the positions of the stops in the array
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 stops.reverse
-p stops.reverse
+# p stops.reverse
 
 # Print out all the stops using a for loop
  stops = [
@@ -55,7 +57,7 @@ p stops.reverse
  ]
 
  for stop in stops
-   p "#{stop} is here"
+    "#{stop} is here"
  end
 
 ############# EXERCISE B
@@ -118,18 +120,79 @@ users = {
 
 
 # Get Jonathan's Twitter handle (i.e. the string "jonnyt")
-  p users["Jonathan"][:twitter]
+   users["Jonathan"][:twitter]
   # Get Erik's hometown
-  p users["Erik"][:home_town]
+   users["Erik"][:home_town]
   # Get the array of Erik's lottery numbers
-  p users["Erik"][:lottery_numbers]
+   users["Erik"][:lottery_numbers]
   # Get the type of Avril's pet Monty
-  p users.dig("Avril", :pets)[0][:species]
+   users.dig("Avril", :pets)[0][:species]
+   users["Avril"][:pets][0][:species]
+
 # Get the smallest of Erik's lottery numbers
-p users["Erik"][:lottery_numbers].min
+  users["Erik"][:lottery_numbers].min()
 
 # Return an array of Avril's lottery numbers that are even
+  def even_numbers(numbers)
+    even_numbers_array = []
+      for number in numbers
+        if(number.even?)
+          even_numbers_array << numnber
+        end
+        end
+        return even_numbers_array
+      end
+    even_numbers(users["Avril"][:lottery_numbers])
+
 # Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+users["Erik"][:lottery_numbers].push(7)
+
 # Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+users["Erik"][:home_town].replace "Edinburgh"
+
 # Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets]( name: "Fluffy", species: "dog" )
+
 # Add another person to the users hash
+new_user = {
+  "Emily" => {
+
+  }
+}
+users.merge!(new_user)
+# or
+users["Emily"] = {
+
+}
+
+
+##########  EXERCISE C
+
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+
+united_kingdom[1][:capital] = "Cardiff"
+
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+# 3. Use a loop to print the names of all the countries in the UK.
+
+# 4. Use a loop to find the total population of the UK.
